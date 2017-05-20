109f27025c52cec55b1f19de038f30dfd59e4505
mike-ddg <mike@doreydesigngroup.com>
2017-05-19 18:29:08 -0700

    floating header development

--- themes/digital_foundation/js/app.js
+++ themes/digital_foundation/js/app.js
@@ -1,1 +1,171 @@
 //$(document).foundation();
+
+/// header effect ////
+
+let last_scroll_pos = 0;
+let scrolling = false;
+
+function headerToggle( sPosition ) {
+	let headerEle = document.querySelector('.region-header');
+	let contentEle = document.getElementById('content');
+	if (sPosition > 500)  {
+		if (!headerEle.classList.contains('header-scroll')) {
+			headerEle.classList.add('header-scroll');
+			contentEle.classList.add('content-scroll');
+		}
+	} else if (sPosition < 500) {
+		if (headerEle.classList.contains('header-scroll')) {
+			headerEle.classList.remove('header-scroll');
+			contentEle.classList.add('content-scroll');
+		}
+	}
+	
+}
+
+function logoToggle( sPosition ) {
+	let logoEle = document.querySelector('.site-logo');
+	if (sPosition > 500)  {
+		if (!logoEle.classList.contains('logo-scroll')) {
+			logoEle.classList.add('logo-scroll');
+		}
+	} else if (sPosition < 500) {
+		if (logoEle.classList.contains('logo-scroll')) {
+			logoEle.classList.remove('logo-scroll');
+		}
+	}
+}
+
+window.onscroll = function (){
+	last_scroll_pos = window.scrollY;
+	if ( !scrolling ) {
+		
+		window.requestAnimationFrame(function() {
+      	headerToggle(last_scroll_pos);
+      	logoToggle(last_scroll_pos);
+      	scrolling = false;
+    	});
+		
+    }
+	scrolling = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
